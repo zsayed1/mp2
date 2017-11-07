e25077f9a209c5aef5a654cb40aab07507976f25
Zeshan Sayed <zsayed1@hawk.iit.edu>
2017-11-07 02:02:21 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -8,7 +8,7 @@
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
                 <style>
                         body {
-                                background-image: url("aws_back.jpg");
+                                
                                 background-repeat: no-repeat;
                         }
                         .first_div{
