9ddbd89698acd9af364dca68b115a84483caee46
zsayed1 <zsayed1@hawk.iit.edu>
2017-11-08 23:52:43 -0600

    test updated with topics

--- test_1.php
+++ test_1.php
@@ -1,9 +1,16 @@
 <?php
-require 'vendor/atoload.php'
-
-echo" Hello world hi there"
-
-$sqs = new Aws/Sns/SnsClient([
+require 'vendor/autoload.php';
+$sqs = new Aws\Sns\SnsClient([
     'version' => 'latest',
     'region' => 'us-west-2'
 ]);
+
+
+
+$result = $sqs->listTopics([
+
+]);
+
+print_r($result['Topics']);
+print_r($result['Topics'][0]['TopicArn']);
+?>
