de8ab9d50b89042bc0d52cb33ff84d55a55dc96b
zsayed1 <zsayed1@hawk.iit.edu>
2017-11-09 00:09:43 -0600

    added subscription

--- test_1.php
+++ test_1.php
@@ -1,12 +1,14 @@
 <?php
 require 'vendor/autoload.php';
+
+// Creatinhg SNS client
 $sqs = new Aws\Sns\SnsClient([
     'version' => 'latest',
-    'region' => 'us-west-2'
+    'region' => 'us-west-2' //regionus0west-2
 ]);
 
 
-
+//creating variable to store the list of Topic arn
 $result = $sqs->listTopics([
 
 ]);
@@ -14,5 +16,12 @@ $result = $sqs->listTopics([
 print_r($result['Topics']);
 $topicarn=($result['Topics'][0]['TopicArn']);
 echo "Your Topic Arn: . $topicarn";
+
+//subscribing sns
+$sqs = $client->subscribe([
+    'Endpoint' => '+16507978341',
+    'Protocol' => 'sms',
+    'TopicArn' => $topicarn, 
+]);
 
 ?>
